ae80435c8bd998c77a0f4c874b6a0403a87854b3
majormalfunk <32271489+majormalfunk@users.noreply.github.com>
2021-04-10 21:00:52 +0300

    Changes and additions to run final tests

--- notebooks/metrics.py
+++ notebooks/metrics.py
@@ -31,22 +31,3 @@ def evaluate(tp_l, tn_l, fp_l, fn_l):
     return {'tp': tp, 'tn': tn, 'fp': fp, 'fn': fn,
         'precision': precision, 'recall': recall, 'f_score': f_score}
 
-def evaluateX(output, target, thresh):
-    '''
-    Parameters:
-    - output needs to be a list (of tensors) that has been sigmoided
-    - target needs to be a list (of tensors) of 0's and 1's
-    - thresh is a float [0, 1) indicating a threshold over which is considered as 1
-    '''
-    out = output > thresh
-    tgt = target > thresh
-    tp = int((out & tgt).sum())
-    tn = int(((~out) & (~tgt)).sum())
-    fp = int((out & (~tgt)).sum())
-    fn = int(((~out) & tgt).sum())
-    precision = (tp / (tp + fp + 1e-12))
-    recall = (tp / (tp + fn + 1e-12))
-    f_score = (2 * precision * recall)/(precision + recall + 1e-12)
-
-    return {'tp': tp, 'tn': tn, 'fp': fp, 'fn': fn,
-        'precision': precision, 'recall': recall, 'f_score': f_score}
--- notebooks/test.py
+++ notebooks/test.py
@@ -17,22 +17,15 @@ def test_time(start_time, end_time):
     elapsed_secs = int(elapsed_time - (elapsed_mins * 60.0))
     return elapsed_mins, elapsed_secs
 
-def test_model(device, model, model_name, criterion, num_labels, dataloader):
-    print(f'Start evaluating/testing model {model_name}')
-    test_losses = []
+def test_model(device, model, model_name, num_labels, dataloader, itemids):
+    print(f'Start testing model {model_name}')
     model.eval()
     steps = 0
-    all_batch_losses = []
-    scores = []
-    # totals = pd.DataFrame(0, columns = list(range(0,126)), index = ['TP', 'TN', 'FP', 'FN'])
-    totals = torch.zeros([4, 126], dtype = torch.int32)
-    SCORE_INTERVAL = 10
     ALIVE_INTERVAL = 100
+    results = []
     
     with torch.no_grad():
         test_start_time = time.time()
-        total_loss = 0
-        batch_losses = []
         for step, batch in enumerate(dataloader):
             batch_start_time = time.time()
             batch = tuple(t.to(device) for t in batch)
@@ -41,55 +34,27 @@ def test_model(device, model, model_name, criterion, num_labels, dataloader):
             # Forward pass
             outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
             logits = outputs[0]
-            loss = criterion(logits.view(-1, num_labels),b_labels.type_as(logits).view(-1,num_labels)) #convert labels to float for calculation
 
-            # Update totals
-            hits = count_hits(torch.sigmoid(logits), b_labels, 0.5)
-            tp = hits['tp']
-            tn = hits['tn']
-            fp = hits['fp']
-            fn = hits['fn']
-            totals[0] = totals[0] + torch.sum(tp, 0).cpu().detach().numpy()
-            totals[1] = totals[1] + torch.sum(tn, 0).cpu().detach().numpy()
-            totals[2] = totals[2] + torch.sum(fp, 0).cpu().detach().numpy()
-            totals[3] = totals[3] + torch.sum(fn, 0).cpu().detach().numpy()
+            prediction = logits > 0.5
+            results.extend(prediction)
 
-            # Evaluate results
-            if steps % SCORE_INTERVAL == 0 or steps == len(dataloader):
-                score = evaluate(tp, tn, fp, fn)
-                score['epoch'] = 1
-                score['batch'] = step
-                scores.append(score)
-
-            total_loss += loss.item()
             steps += 1
             batch_end_time = time.time() 
             
-            # Loss check
-            loss_check = total_loss / (step+1)
-            batch_losses.append(loss_check)
+            # Alive check
             batch_mins, batch_secs = test_time(batch_start_time, batch_end_time)
             if steps % ALIVE_INTERVAL == 0 or steps == len(dataloader):
                 print(f'Step {step} | Batch time: {batch_mins}m {batch_secs}s')
-                print(f'\tLoss check: {loss_check:.3f}')
         
-        test_loss = total_loss / len(dataloader)
-        test_losses.append(test_loss)
-        all_batch_losses.append(batch_losses)
-            
         test_end_time = time.time()
             
         test_mins, test_secs = test_time(test_start_time, test_end_time)
         print(f'Testing Time: {test_mins}m {test_secs}s')
-        print(f'\tTest Loss: {test_loss:.3f}')
         
-    run_id = model_name
-    pdScores = pd.DataFrame(scores)
-    pdScores.to_csv(f'notebooks/scores/scores_{run_id}.csv', index = False)
-    batchLossDf = pd.DataFrame(all_batch_losses)
-    batchLossDf.to_csv(f'notebooks/scores/batch_losses_{run_id}.csv', index = False)
-    pdTotals = pd.DataFrame(totals.tolist(), index = ['TP', 'TN', 'FP', 'FN'])
-    pdTotals.T.to_csv(f'notebooks/scores/totals_{run_id}.csv', index = False)
+    run_id = sys.argv[2]
+    dfResults = pd.DataFrame(results)
+    dfResults['id'] = itemids
+    dfResults.to_csv(f'notebooks/test_results_{run_id}.csv', index = False)
 
 
 def main():
@@ -101,20 +66,23 @@ def main():
     test_data_loader_name = f'notebooks/data-loaders/{sys.argv[1]}'
     model_name = f'notebooks/models/{sys.argv[2]}'
 
-    print(f'Load the evaluation/test data from {test_data_loader_name}')
+    print(f'Load the test data from {test_data_loader_name}')
     test_dataloader = torch.load(test_data_loader_name)
 
     print(f'Load the model from {model_name}')
-    # model = BertForSequenceClassification.from_pretrained(model_name, num_labels = NUM_LABELS)
+
     model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels = NUM_LABELS)
     if device == 'cuda':
       model.load_state_dict(torch.load(model_name))
     else:
       model.load_state_dict(torch.load(model_name, map_location = torch.device('cpu')))
     model.to(device)
-    criterion = BCEWithLogitsLoss()
 
-    test_model(device, model, model_name, criterion, NUM_LABELS, test_dataloader)
+    # load csv
+    df = pd.read_csv(f'notebooks/reuters-csv/test.csv', delimiter=';')
+    itemids = df['id']
+
+    test_model(device, model, model_name, NUM_LABELS, test_dataloader, itemids)
     print('Finished')
 
 
